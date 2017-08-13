e6a3622d60db089b8afbc3be22a9700bc046bb3d
Juri Gagarov <mapman100@yandex.com>
2017-08-13 15:15:07 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,14 @@
+{
+"moduleId" : "mapman100/Here_WEGO",
+"title" : "HERE_WEGO",
+"subtitle" : "wego.here.com",
+"backButton" : true,
+"enableGPS" : true,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : true,
+"shouldOverrideUrlLoading1" : "http://wego.here.com",
+"shouldOverrideUrlLoading2" : "https://wego.here.com",
+"loadUrl" : "https://mobile.here.com/?map=#lat#,#lng#,#zoom#,normal&x=ep",
+"browserLaunchLink" : "https://mobile.here.com/?map=#lat#,#lng#,#zoom#,normal&x=ep"
+}
